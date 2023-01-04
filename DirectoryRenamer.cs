b4a8003818f5d12ee92eef4d6969429eca51d837
Ignacio Benito Soto <ignaciobenitosoto@gmail.com>
2023-01-05 00:44:32 +0100

    Add project files.

---
+++ DirectoryRenamer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    public static class DirectoryRenamer
+    {
+        public static void RenameDirectoryTree(string path, Func<string, string> renamingRule)
+        {
+            var di = new DirectoryInfo(path);
+            RenameDirectoryTree(di, renamingRule);
+        }
+
+        public static void RenameDirectoryTree(DirectoryInfo directory, Func<string, string> renamingRule)
+        {
+            InternalRenameDirectoryTree(directory, renamingRule);
+
+            var currentName = directory.Name;
+            var newName = renamingRule(currentName);
+            if (currentName != newName)
+            {
+                var newDirname = Path.Combine(directory.Parent.FullName, newName);
+                directory.MoveTo(newDirname);
+            }
+        }
+
+        static void InternalRenameDirectoryTree(DirectoryInfo di, Func<string, string> renamingRule)
+        {
+            foreach (var item in di.GetFileSystemInfos())
+            {
+                var subdir = item as DirectoryInfo;
+                if (subdir != null)
+                {
+                    InternalRenameDirectoryTree(subdir, renamingRule);
+
+                    var currentName = subdir.Name;
+                    var newName = renamingRule(currentName);
+                    if (currentName != newName)
+                    {
+                        var newDirname = Path.Combine(subdir.Parent.FullName, newName);
+                        subdir.MoveTo(newDirname);
+                    }
+                }
+
+                var file = item as FileInfo;
+                if (file != null)
+                {
+                    var currentName = Path.GetFileNameWithoutExtension(file.Name);
+                    var newName = renamingRule(currentName);
+                    if (currentName != newName)
+                    {
+                        var newFilename = Path.Combine(file.DirectoryName, newName + file.Extension);
+                        file.MoveTo(newFilename);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenamer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryRenamer.RenameDirectoryTree(@"C:\Test\FolderMatchALevel", name => name.Replace("MatchA", "AMatch"));
+        }
+    }
+}
